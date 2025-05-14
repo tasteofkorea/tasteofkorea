76c6e7849ab858ad1b5ba07a824263d7ef7c5d91
12hyeon <hyeonjeong1212@naver.com>
2025-05-14 14:55:30 +0900

    해당 알러지 조회

--- src/main/java/com/example/tasteofkorea/repository/FoodFilterListRepository.java
+++ src/main/java/com/example/tasteofkorea/repository/FoodFilterListRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface FoodFilterListRepository extends JpaRepository<FoodFilterListEntity, Long> {
-
+    List<FoodFilterListEntity> findByFoodId(Long foodId);
 
 
 }
