dd2e889ba58e90a666071fb1d6afeaeae39907d9
LJH <qufwnsgh3@naver.com>
2025-06-17 20:51:09 +0900

    FastApiService.java 수정

--- src/main/java/com/example/tasteofkorea/service/FastApiService.java
+++ src/main/java/com/example/tasteofkorea/service/FastApiService.java
@@ -1,6 +1,7 @@
 package com.example.tasteofkorea.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
